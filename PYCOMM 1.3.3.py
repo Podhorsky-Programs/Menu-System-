5923cd7590983e6955d4c526360e78a25387f754
Blake Podhorsky <blakepodhorsky@gmail.com>
2025-12-03 19:48:59 -0600

    Enhance PC part selector and update documentation
    Updated version log and added PC part selection functionality.

--- PYCOMM 1.3.3.py
+++ PYCOMM 1.3.3.py
@@ -1,4 +1,5 @@
-#!/user/bin/env python3
+
+.#!/user/bin/env python3
 # ----------------------------------------------------------------------------------------------------------
 # script: PYCOMM V 1.3.3.py
 # first created: 2/7/2025
@@ -29,6 +30,7 @@
 #              11/12/2025 added Read.ME,transferd PYCOMM to Github
 #              11/14/2025 1.3.2 additons to word game, slight change to documentation. Updated Read.ME
 #              11/29/2015 PYCOMM 1.3.3- Added PC part selector, updated menu(option A), updated functions
+#              12/3/2025 PYCOMM 1.4.0- Moddification of PC part picker. adjustments to documentation
 # ------------------------------------------------------------------------------------------------------------
 
 #imports
@@ -60,6 +62,7 @@ def menu():
 def pc_prt_select():
     # this function takes user imputs to create an optimal PC based of use and budget
     # this is modeled after a previous project i made, however it adds RAM and a CPU and a motherboard
+    # all prciing info is based of off Amazon.com, these prices are the prices of december of 2025
     budget = num(input("Whats your budget(5000, 3500, 2500, 1500): "))
     pc_use = str(input("What is the PC for(gaming- streaming- editing): "))
     your_pc == str("Your PC parts include a " + gpu + ", a " + cpu + ", a " ram + " and a "  + mthr_brd)
@@ -70,21 +73,21 @@ def pc_prt_select():
     if pc_use == "Gaming" or pc_Use == "gaming":
         if budget == 5000:
              gpu = "ROG Astral GeForce RTX 5090 OC edition"
-             cpu = "AMD RYZEN 7 9800X3D"
+             cpu = "AMD RYZEN 7 7700X"
              ram = "Kingston Fury beast DDR5 64GB 6400MT/s"
-             mthr_brd = "TUF gaming B850- Plus WIFI AMD AM5 ATX"
+             mthr_brd = "ASUS ROG Strix X870E-E Gaming Wifi"
              print(your_pc)
         elif budget == 3500:
              gpu = "AMD Radeon RX 7900 XT phantom OC"
              cpu = "AMD RYZEN 7 9800X3D"
-             ram = "Kingston Fury beast DDR5 64GB 6400MT/s"
+             ram = "Kingston Fury beast DDR5 64GB 6400MTz"
              mthr_brd = "TUF gaming B850- Plus WIFI AMD AM5 ATX"
              print(your_pc)
         elif budget == 2500:
-            gpu =
-            cpu =
-            ram =
-            mthr_brd =
+            gpu = "Radeon RX 9060 XT OC"
+            cpu = "Ryzen 5 9600X"
+            ram = "T-FORCE DELTA DDR% 32GB 6000 MHz"
+            mthr_brd = "ASUS ROG Strix X870E-E Gaming Wifi"
             print(your_pc)
         elif budget == 1500:
 
@@ -410,6 +413,7 @@ mainline_pw()
 # welcome to the rattle (end of program)
 
 # ----------------------------------------------------------------------------------------------------------
+
 
 
 
